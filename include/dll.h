d5a30ce1b9465fff0421fcf06af8dafee0ce253f
Trent Gamblin <trent@nooskewl.ca>
2015-07-11 15:45:44 -0600

    Works on Linux, plus minor general improvements

--- include/dll.h
+++ include/dll.h
@@ -4,10 +4,16 @@
 #include "Nooskewl_Engine/map.h"
 #include "Nooskewl_Engine/map_logic.h"
 
+#ifdef _MSC_VER
+#define MONSTERRPG3_EXPORT __declspec(dllexport)
+#else
+#define MONSTERRPG3_EXPORT __attribute__((visibility("default")))
+#endif
+
 using namespace Nooskewl_Engine;
 
 extern "C" {
-	__declspec(dllexport) Map_Logic *get_map_logic(Map *map);
+	MONSTERRPG3_EXPORT Map_Logic *get_map_logic(Map *map);
 }
 
 #endif // DLL_H
