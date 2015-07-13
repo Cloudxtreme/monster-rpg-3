e5bc623aafa839e4105daa2af51f9cdf9196330d
Trent Gamblin <trent@nooskewl.ca>
2015-07-13 04:33:27 -0600

    Add linux_cmake.sh

---
+++ linux_cmake.sh
@@ -0,0 +1,1 @@
+cmake .. -DCMAKE_BUILD_TYPE=Release -DTRENTS_BUILD=on -D"USER_INCLUDE_PATH=/home/trent/code/include;/home/trent/code/Nooskewl_Engine/include" -DUSER_LIBRARY_PATH=/home/trent/code/lib
