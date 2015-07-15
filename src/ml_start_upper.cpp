da256d5a2a05dcc76187a34aef362a39c8a930e5
= <=>
2015-07-15 13:37:00 -0600

    Update for updated maps

--- src/ml_start_upper.cpp
+++ src/ml_start_upper.cpp
@@ -12,6 +12,6 @@ void ML_start_upper::trigger(Map_Entity *entity)
 	Point<int> collide_pos;
 	if (entity->get_id() == 0 && entity->tiles_collide(Point<int>(22, 3), Size<int>(2, 2), collide_pos)) {
 		entity->stop();
-		noo.map->change_map("start.map", Point<int>(30, 2), S);
+		noo.map->change_map("start.map", Point<int>(19, 4), S);
 	}
 }
