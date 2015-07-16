abe2bab624e4ddd9eea5b25df7a9d29c0c15bfeb
= <=>
2015-07-16 06:59:36 -0600

    Less padding on prompt

--- src/ml_start.cpp
+++ src/ml_start.cpp
@@ -37,15 +37,15 @@ void ML_start::trigger(Map_Entity *entity)
 			noo.player->disable_input();
 			MO3_Widget *main_widget = new MO3_Widget(1.0f, 1.0f);
 			pass = new MO3_Text_Button("Pass", Size<int>(50, -1));
-			pass->set_padding(5, 20, noo.screen_size.h-20-pass->get_height(), 0);
+			pass->set_padding(5, 10, noo.screen_size.h-10-pass->get_height(), 0);
 			pass->set_float_right(true);
 			pass->set_parent(main_widget);
 			yes = new MO3_Text_Button("Yes", Size<int>(50, -1));
-			yes->set_padding(0, 0, noo.screen_size.h-20-yes->get_height(), 0);
+			yes->set_padding(0, 0, noo.screen_size.h-10-yes->get_height(), 0);
 			yes->set_float_right(true);
 			yes->set_parent(main_widget);
 			MO3_Window *bubble = new MO3_Window(105, noo.font->get_height()+padding*2);
-			bubble->set_padding(0, 20, pass->get_padding_top()-5-bubble->get_height(), 0);
+			bubble->set_padding(0, 10, pass->get_padding_top()-5-bubble->get_height(), 0);
 			bubble->set_float_right(true);
 			bubble->set_clear_floats(true);
 			bubble->set_parent(main_widget);
