8fe785ea52a105bd8caa4166989e21282ba63d37
= <=>
2015-07-22 14:25:31 -0600

    Brains can not be loaded from DLL - add Animated_Brain

---
+++ include/brains.h
@@ -0,0 +1,17 @@
+#ifndef BRAINS_H
+#define BRAINS_H
+
+#include "Nooskewl_Engine/main.h"
+#include "Nooskewl_Engine/talk_brain.h"
+
+using namespace Nooskewl_Engine;
+
+class Animated_Brain : public Talk_Brain {
+public:
+	Animated_Brain(std::string name);
+
+	void update();
+	bool save(SDL_RWops *file);
+};
+
+#endif // BRAINS_H
---
+++ src/brains.cpp
@@ -0,0 +1,37 @@
+#include "Nooskewl_Engine/map_entity.h"
+
+#include "brains.h"
+
+Animated_Brain::Animated_Brain(std::string name) :
+	Talk_Brain(name)
+{
+}
+
+void Animated_Brain::update()
+{
+	if (map_entity == 0) {
+		return;
+	}
+
+	Sprite *sprite = map_entity->get_sprite();
+
+	if (!sprite->is_started()) {
+		sprite->start();
+	}
+
+	if (name == "laughing_man") {
+		Uint32 ticks = SDL_GetTicks() % 5000;
+		if (ticks < 500) {
+			sprite->set_animation("laugh");
+		}
+		else {
+			sprite->set_animation("sit_s");
+		}
+	}
+}
+
+bool Animated_Brain::save(SDL_RWops *file)
+{
+	SDL_fprintf(file, "brain=animated_brain,%s\n", name.c_str());
+	return true;
+}
