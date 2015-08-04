9675e72db4b46c68861263cba962596798c4f5c6
Trent Gamblin <trent@nooskewl.ca>
2015-08-04 11:09:08 -0600

    Load the Door brains after the sprite because set_brain needs it

--- src/ml_start_upper.cpp
+++ src/ml_start_upper.cpp
@@ -9,41 +9,41 @@ void ML_start_upper::start()
 
 	if (noo.last_map_name != "--LOADED--") {
 		Map_Entity *door1 = new Map_Entity("door");
-		Door_Brain *door1_brain = new Door_Brain();
-		door1->set_brain(door1_brain);
 		door1->load_sprite("door");
+		Door_Brain *door1_brain = new Door_Brain(false);
+		door1->set_brain(door1_brain);
 		door1->set_position(Point<int>(3, 7));
 		door1->set_shadow_type(Map_Entity::SHADOW_TRANSLUCENT_COPY);
 		door1->set_solid(false);
 
 		Map_Entity *door2 = new Map_Entity("door");
-		Door_Brain *door2_brain = new Door_Brain();
-		door2->set_brain(door2_brain);
 		door2->load_sprite("door");
+		Door_Brain *door2_brain = new Door_Brain(false);
+		door2->set_brain(door2_brain);
 		door2->set_position(Point<int>(9, 7));
 		door2->set_shadow_type(Map_Entity::SHADOW_TRANSLUCENT_COPY);
 		door2->set_solid(false);
 
 		Map_Entity *door3 = new Map_Entity("door");
-		Door_Brain *door3_brain = new Door_Brain();
-		door3->set_brain(door3_brain);
 		door3->load_sprite("door");
+		Door_Brain *door3_brain = new Door_Brain(false);
+		door3->set_brain(door3_brain);
 		door3->set_position(Point<int>(15, 7));
 		door3->set_shadow_type(Map_Entity::SHADOW_TRANSLUCENT_COPY);
 		door3->set_solid(false);
 
 		Map_Entity *door4 = new Map_Entity("door");
-		Door_Brain *door4_brain = new Door_Brain();
-		door4->set_brain(door4_brain);
 		door4->load_sprite("door");
+		Door_Brain *door4_brain = new Door_Brain(false);
+		door4->set_brain(door4_brain);
 		door4->set_position(Point<int>(3, 12));
 		door4->set_shadow_type(Map_Entity::SHADOW_TRANSLUCENT_COPY);
 		door4->set_solid(false);
 
 		Map_Entity *door5 = new Map_Entity("door");
-		Door_Brain *door5_brain = new Door_Brain();
-		door5->set_brain(door5_brain);
 		door5->load_sprite("door");
+		Door_Brain *door5_brain = new Door_Brain(false);
+		door5->set_brain(door5_brain);
 		door5->set_position(Point<int>(21, 12));
 		door5->set_shadow_type(Map_Entity::SHADOW_TRANSLUCENT_COPY);
 		door5->set_solid(false);
