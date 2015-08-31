9229afa10d7858f8f1770a3196d8b58e1c17efff
Trent Gamblin <trent@nooskewl.ca>
2015-08-31 11:48:56 -0600

    Add cabbages

--- src/ml_town.cpp
+++ src/ml_town.cpp
@@ -1,8 +1,19 @@
 #include <Nooskewl_Engine/Nooskewl_Engine.h>
 
+#include "brains.h"
 #include "ml.h"
 #include "ml_town.h"
 #include "tweens.h"
+
+static void add_cabbage(int x, int y)
+{
+	std::string name = rand() % 2 ? "cabbage" : "rotten_cabbage";
+	Map_Entity *cabbage = new Map_Entity(name);
+	cabbage->set_brain(new Item_Brain(name, 1));
+	cabbage->load_sprite(name);
+	cabbage->set_position(Point<int>(x, y));
+	noo.map->add_entity(cabbage);
+}
 
 ML_town::ML_town()
 {
@@ -13,6 +24,76 @@ void ML_town::start(bool been_here_before)
 	noo.play_music("town.mml");
 
 	if (!been_here_before) {
+		for (int y = 17; y <= 17; y++) {
+			for (int x = 27; x <= 32; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 13; y <= 16; y++) {
+			for (int x = 32; x <= 32; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 13; y <= 17; y++) {
+			for (int x = 36; x <= 36; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 13; y <= 17; y++) {
+			for (int x = 41; x <= 41; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 17; y <= 17; y++) {
+			for (int x = 37; x <= 40; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 17; y <= 17; y++) {
+			for (int x = 46; x <= 50; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 13; y <= 17; y++) {
+			for (int x = 45; x <= 45; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 26; y++) {
+			for (int x = 27; x <= 32; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 27; y <= 30; y++) {
+			for (int x = 32; x <= 32; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 30; y++) {
+			for (int x = 36; x <= 36; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 30; y++) {
+			for (int x = 41; x <= 41; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 26; y++) {
+			for (int x = 37; x <= 40; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 30; y++) {
+			for (int x = 45; x <= 45; x++) {
+				add_cabbage(x, y);
+			}
+		}
+		for (int y = 26; y <= 26; y++) {
+			for (int x = 46; x <= 50; x++) {
+				add_cabbage(x, y);
+			}
+		}
 	}
 }
 
