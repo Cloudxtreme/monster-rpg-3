5eec4f5d00d725c4a5b86c591fae7a44bac89648
Trent Gamblin <trent@nooskewl.ca>
2015-09-22 06:58:30 -0600

    Add Alfred who gives you something for apples

--- src/ml_cabbagetown.cpp
+++ src/ml_cabbagetown.cpp
@@ -1,8 +1,10 @@
 #include <Nooskewl_Engine/Nooskewl_Engine.h>
 
 #include "brains.h"
+#include "gui.h"
 #include "ml.h"
 #include "ml_cabbagetown.h"
+#include "monster-rpg-3.h"
 #include "tweens.h"
 
 const int num_cabbages = 68;
@@ -119,6 +121,15 @@ void ML_cabbagetown::start(bool been_here_before)
 				add_red_cabbage(x, y);
 			}
 		}
+
+		// people
+		Map_Entity *alfred = new Map_Entity("alfred");
+		alfred->load_sprite("alfred");
+		alfred->set_position(Point<int>(29, 10));
+		alfred->set_direction(S);
+		alfred->set_sitting(true);
+
+		noo.map->add_entity(alfred);
 	}
 }
 
@@ -139,6 +150,81 @@ void ML_cabbagetown::update()
 {
 }
 
+struct Apple_Callback_Data {
+	int ms1;
+	int ms2;
+	bool ms1_complete;
+	bool ms2_complete;
+	Map_Entity *alfred;
+};
+
+static Apple_Callback_Data acd;
+
+static void face_alfred_south(void *data)
+{
+	Map_Entity *alfred = static_cast<Map_Entity *>(data);
+	alfred->set_direction(S);
+}
+
+static void apple_callback(void *data)
+{
+	Multiple_Choice_GUI::Callback_Data *d = static_cast<Multiple_Choice_GUI::Callback_Data *>(data);
+	Apple_Callback_Data *acd = static_cast<Apple_Callback_Data *>(d->userdata);
+	if (d->choice == 0) { // yes
+		Stats *stats = noo.player->get_stats();
+		stats->inventory->remove("apple");
+
+		if (acd->ms1_complete) {
+			noo.set_milestone(acd->ms2, true);
+			give_item(noo.player, "sleeping_bag", 1, -1);
+		}
+		else {
+			noo.set_milestone(acd->ms1, true);
+		}
+	}
+	face_alfred_south(acd->alfred);
+}
+
 void ML_cabbagetown::activate(Map_Entity *activator, Map_Entity *activated)
 {
+	if (activator == noo.player && activated->get_name() == "alfred") {
+		activated->set_direction(get_facing_direction(activator, activated));
+
+		acd.ms1 = noo.milestone_name_to_number("alfred apple 1");
+		acd.ms2 = noo.milestone_name_to_number("alfred apple 2");
+		acd.ms1_complete = noo.check_milestone(acd.ms1);
+		acd.ms2_complete = noo.check_milestone(acd.ms2);
+		acd.alfred = activated;
+
+		Stats *stats = noo.player->get_stats();
+		int apple_index = stats->inventory->find("apple");
+
+		std::vector<std::string> choices;
+		choices.push_back(TRANSLATE("Yes")END);
+		choices.push_back(TRANSLATE("No")END);
+
+		if (acd.ms2_complete) {
+			noo.map->add_speech("name=Alfred|" + TRANSLATE("I'm not starving.")END, face_alfred_south, activated);
+		}
+		else if (acd.ms1_complete) {
+			if (apple_index < 0) {
+				noo.map->add_speech("name=Alfred|" + TRANSLATE("Hey hey! Got any apples? I love apples.")END, face_alfred_south, activated);
+			}
+			else {
+				Multiple_Choice_GUI *gui = new Multiple_Choice_GUI(TRANSLATE("Got another apple? I'll make it worth it.")END, choices, apple_callback, &acd);
+				gui->start();
+				noo.guis.push_back(gui);
+			}
+		}
+		else {
+			if (apple_index < 0) {
+				noo.map->add_speech("name=Alfred|" + TRANSLATE("Hey hey! Got any apples? I love apples.")END, face_alfred_south, activated);
+			}
+			else {
+				Multiple_Choice_GUI *gui = new Multiple_Choice_GUI(TRANSLATE("Oh! Ohhh! Can I have that apple?")END, choices, apple_callback, &acd);
+				gui->start();
+				noo.guis.push_back(gui);
+			}
+		}
+	}
 }
