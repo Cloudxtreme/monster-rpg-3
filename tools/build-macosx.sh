93c6ca79569e68f9383f8431ed243987bdb80118
Trent Gamblin <trent@nooskewl.ca>
2015-12-02 15:32:44 -0700

    Build/build script changes for OS X

--- tools/build-macosx.sh
+++ tools/build-macosx.sh
@@ -1,12 +1,14 @@
 cd tgui3
 git pull --rebase
 cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libtgui3.dylib
 cp libtgui3.dylib ~/code/mo3/MonsterRPG3.app/Contents/MacOS
 cd ../../Nooskewl_Engine
 git pull --rebase
 cd build
+cmake .. -DLOAD_FROM_FILESYSTEM=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libNooskewl_Engine.dylib
 ~/bin/codesign.sh Nooskewl_Runner
@@ -14,7 +16,7 @@ cp libNooskewl_Engine.dylib Nooskewl_Runner ~/code/mo3/MonsterRPG3.app/Contents/
 cd ../../monster-rpg-3
 git pull --rebase
 cd build
-cmake .. -DDEMO=off
+cmake .. -DDEMO=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libgame.dylib
 cp libgame.dylib ~/code/mo3/MonsterRPG3.app/Contents/MacOS
--- tools/build_demo-macosx.sh
+++ tools/build_demo-macosx.sh
@@ -1,12 +1,14 @@
 cd tgui3
 git pull --rebase
 cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libtgui3.dylib
 cp libtgui3.dylib ~/code/mo3_demo/MonsterRPG3Demo.app/Contents/MacOS
 cd ../../Nooskewl_Engine
 git pull --rebase
 cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libNooskewl_Engine.dylib
 ~/bin/codesign.sh Nooskewl_Runner
@@ -14,7 +16,7 @@ cp libNooskewl_Engine.dylib Nooskewl_Runner ~/code/mo3_demo/MonsterRPG3Demo.app/
 cd ../../monster-rpg-3
 git pull --rebase
 cd build
-cmake .. -DDEMO=on
+cmake .. -DDEMO=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libgame.dylib
 cp libgame.dylib ~/code/mo3_demo/MonsterRPG3Demo.app/Contents/MacOS
