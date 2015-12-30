348c9746e6c42cdb8a37d13b8adc6c2acd0bb3f6
Trent Gamblin <trent@nooskewl.ca>
2015-12-30 01:18:53 -0700

    Add spaces to .app names (Mac)

--- tools/build-macosx.sh
+++ tools/build-macosx.sh
@@ -4,7 +4,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libtgui3.dylib
-cp libtgui3.dylib ~/code/mo3/MonsterRPG3.app/Contents/MacOS
+cp libtgui3.dylib ~/code/mo3/"Monster RPG 3.app"/Contents/MacOS
 cd ../../Nooskewl_Engine
 git pull --rebase
 cd build
@@ -12,17 +12,18 @@ cmake .. -DLOAD_FROM_FILESYSTEM=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLO
 make -j2
 ~/bin/codesign.sh libNooskewl_Engine.dylib
 ~/bin/codesign.sh Nooskewl_Runner
-cp libNooskewl_Engine.dylib Nooskewl_Runner ~/code/mo3/MonsterRPG3.app/Contents/MacOS
+cp libNooskewl_Engine.dylib ~/code/mo3/"Monster RPG 3.app"/Contents/MacOS
+cp Nooskewl_Runner ~/code/mo3/"Monster RPG 3.app"/Contents/MacOS
 cd ../../monster-rpg-3
 git pull --rebase
 cd build
 cmake .. -DDEMO=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libgame.dylib
-cp libgame.dylib ~/code/mo3/MonsterRPG3.app/Contents/MacOS
+cp libgame.dylib ~/code/mo3/"Monster RPG 3.app"/Contents/MacOS
 cd ../../monster-rpg-3-data
 git pull --rebase
 ../Nooskewl_Engine/tools/cpa/mkcpa-osx.sh ../mo3/data.cpa
 cd ../mo3
 rm data.cpa.uncompressed
-mv data.cpa MonsterRPG3.app/Contents/Resources
+mv data.cpa "Monster RPG 3.app"/Contents/Resources
--- tools/build_demo-macosx.sh
+++ tools/build_demo-macosx.sh
@@ -4,7 +4,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libtgui3.dylib
-cp libtgui3.dylib ~/code/mo3_demo/MonsterRPG3Demo.app/Contents/MacOS
+cp libtgui3.dylib ~/code/mo3_demo/"Monster RPG 3 Demo.app"/Contents/MacOS
 cd ../../Nooskewl_Engine
 git pull --rebase
 cd build
@@ -12,17 +12,18 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libNooskewl_Engine.dylib
 ~/bin/codesign.sh Nooskewl_Runner
-cp libNooskewl_Engine.dylib Nooskewl_Runner ~/code/mo3_demo/MonsterRPG3Demo.app/Contents/MacOS
+cp libNooskewl_Engine.dylib ~/code/mo3_demo/"Monster RPG 3 Demo.app"/Contents/MacOS
+cp Nooskewl_Runner ~/code/mo3_demo/"Monster RPG 3 Demo.app"/Contents/MacOS
 cd ../../monster-rpg-3
 git pull --rebase
 cd build
 cmake .. -DDEMO=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7
 make -j2
 ~/bin/codesign.sh libgame.dylib
-cp libgame.dylib ~/code/mo3_demo/MonsterRPG3Demo.app/Contents/MacOS
+cp libgame.dylib ~/code/mo3_demo/"Monster RPG 3 Demo.app"/Contents/MacOS
 cd ../../monster-rpg-3-demo-data
 git pull --rebase
 ../Nooskewl_Engine/tools/cpa/mkcpa-osx.sh ../mo3_demo/data.cpa
 cd ../mo3_demo
 rm data.cpa.uncompressed
-mv data.cpa MonsterRPG3Demo.app/Contents/Resources
+mv data.cpa "Monster RPG 3 Demo.app"/Contents/Resources
